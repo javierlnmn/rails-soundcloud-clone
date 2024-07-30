54cdbb27649a19ec4757f81c7063b9ea9515649c
javierlnmn <jlm03123@gmail.com>
2024-07-30 12:05:58 +0200

    Posts controllers and various advances

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    resources :posts
+    devise_for :users
+    get 'pages/home'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "pages#home"
+    authenticated :user do
+        root "posts#index", as: :authenticated_user_root
+    end
+
+    # Defines the root path route ("/")
+    root "pages#home"
 end
